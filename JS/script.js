8303c5e776fd64f275aa63db3ab26db49075dbf6
Serenajandra <serena.jandra@gmail.com>
2022-10-17 19:04:36 +0200

    completata griglia

--- JS/script.js
+++ JS/script.js
@@ -14,19 +14,37 @@
 // <!-- Griglia: -->
 
 // <!-- Aggiungo numerazione progressiva da 1 a 100 sulla griglia 10X10caselle-->
-
+let thisSquare = "";
 let numbers = [];
 for(let i = 1; i < 100 + 1; i++){ 
     console.log([i]);
     thisNumber = [i];
     const row = document.querySelector(".row");
-    const thisSquare = document.createElement ("div")
-    thisSquare.classList.add("col")
+     thisSquare = document.createElement ("div");
+    thisSquare.classList.add("col");
     thisSquare.innerHTML = thisNumber;
     console.log(thisSquare);
     row.append(thisSquare);
+    // row.classList.add("d_hidden");
 }
+
+// const gridContainer = document.querySelector(".container");
+// gridContainer.classList.add("d_hidden");
 
 // <!-- Creo evento bottone con display block sulla griglia di HTML -->
 
+const startBtn = document.querySelector(".generabtn");
+const generaBtnContainer = document.querySelector(".btn_container");
+
+startBtn.addEventListener("click", function(){
+generaBtnContainer.classList.add("")    
+
+})
+
 // <!-- Aggiungo evento su ciascuna cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata. -->
+thisSquare.addEventListener("click", handleSquareClick);
+row.append(thisSquare);
+console.log("thisSquare");
+
+// function handleSquareClick();
+// thisSquare.classList.add("bg_blue");
