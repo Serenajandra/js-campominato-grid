252ebc6acd9aa1215b6ae8a90fdb6573261c54cc
Serenajandra <serena.jandra@gmail.com>
2022-10-17 16:35:49 +0200

    inserito contenuto file html e css

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+// **Consegna**
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+// **Bonus**
+// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+// - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+// - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+// - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe; -->
+
+// <!-- ESECUZIONE -->
+// <!-- Griglia: -->
+
+// <!-- Aggiungo numerazione progressiva da 1 a 100 sulla griglia 10X10caselle-->
+
+
+// <!-- Creo evento bottone con display block sulla griglia di HTML -->
+// <!-- Aggiungo evento su ciascuna cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata. -->
